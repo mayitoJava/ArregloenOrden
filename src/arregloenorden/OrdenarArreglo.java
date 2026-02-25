4d478fad67ac362b39e05193ea7fc0de2cf1aef3
mariovilchis <mariovilchis01@hotmail.com>
2026-02-25 11:28:03 -0600

    NumeroPrimo

--- src/arregloenorden/OrdenarArreglo.java
+++ src/arregloenorden/OrdenarArreglo.java
@@ -6,20 +6,23 @@ public class OrdenarArreglo {
 
         int[] arreglo = {11, 4, 15, 8, 0, 22, 3};
 
-        // Método burbuja
+        // Repite el proceso n-1 veces
         for (int i = 0; i < arreglo.length - 1; i++) {
+            
+            // Recorre el arreglo comparando posiciones consecutivas
             for (int j = 0; j < arreglo.length - 1; j++) {
 
+                // Si el actual es mayor que el siguiente, los intercambia
                 if (arreglo[j] > arreglo[j + 1]) {
-                    int aux = arreglo[j];
-                    arreglo[j] = arreglo[j + 1];
-                    arreglo[j + 1] = aux;
+
+                    int aux = arreglo[j];      // Guarda el valor actual
+                    arreglo[j] = arreglo[j + 1]; // Pasa el siguiente a la izquierda
+                    arreglo[j + 1] = aux;      // Coloca el guardado a la derecha
                 }
             }
         }
 
-        System.out.println("Arreglo ordenado:");
-
+        // Imprime el arreglo ya ordenado
         for (int num : arreglo) {
             System.out.print(num + " ");
         }
